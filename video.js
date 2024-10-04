3c35ea760f45e36712e57360ec59b9c6f455f579
MostafizurRahman199 <fardilshifat199@gmail.com>
2024-10-04 19:17:48 +0600

    modal added

--- video.js
+++ video.js
@@ -4,6 +4,8 @@ function getById(id){
 
 const category_section = getById("category_section");
 const video_section = getById("video_section");
+const no_video_section = getById("no_video_section");
+const my_modal_1 = getById("my_modal_1");
 
 
 const loadCategory = async()=>{
@@ -42,9 +44,9 @@ const btnDesign = async(id)=>{
             let btn = getById(item.category.toLowerCase());
             btn.classList = "btn hover:text-white hover:bg-gradient-to-r hover:from-fuchsia-500 hover:to-cyan-500  rounded-md py-1 px-4  cursor-pointer m-2";
 
-          if(id !== 'all'){
-            allBtn.classList = "btn hover:text-white hover:bg-gradient-to-r hover:from-fuchsia-500 hover:to-cyan-500  rounded-md py-1 px-4  cursor-pointer m-2";
-          }
+                if(id !== 'all'){
+                    allBtn.classList = "btn hover:text-white hover:bg-gradient-to-r hover:from-fuchsia-500 hover:to-cyan-500  rounded-md py-1 px-4  cursor-pointer m-2";
+                }
         }
     })
    
@@ -53,28 +55,26 @@ const btnDesign = async(id)=>{
 
 
 
-const loadCategoryWiseVideo = async(id, id2 )=>{
+const loadCategoryWiseVideo = async(id, id2 ) => {
+    
+    btnDesign(id2);
 
-
-    // alert(id2)
-     btnDesign(id2);
     try {
        if(id === 'all'){
         loadVideo();
        }else{
-        let data = await fetch(`https://openapi.programming-hero.com/api/phero-tube/category/${id}`);
-       
-        data = await data.json();
-        console.log(data);
-        let videos = data.category;
-        if(videos.length){
-        console.log(videos)
-        displayVideos(videos);
-       }else{
-        displayVideos(false);
-       }
+            let data = await fetch(`https://openapi.programming-hero.com/api/phero-tube/category/${id}`);
         
-       }
+            data = await data.json();
+            console.log(data);
+            let videos = data.category;
+
+            if(videos.length){
+                console.log(videos)
+                displayVideos(videos);
+
+            }else{displayVideos(false);}
+        }
     } catch (error) {
         console.log(error);
     }
@@ -84,7 +84,7 @@ const loadCategoryWiseVideo = async(id, id2 )=>{
 const displayCategory = async()=>{
 
    const data = await loadCategory();
-   console.log(data);
+//    console.log(data);
   data.map((item)=>{
        // category_section.classList = "w-11/12 mx-auto flex justify-center items-center my-2 border-2"
        const id2 = item.category.toLowerCase();
@@ -109,6 +109,101 @@ const loadVideo = async ()=>{
     } catch (error) {
         console.log(error);
     }
+}
+
+/**
+ * {
+    "category_id": "1001",
+    "video_id": "aaaa",
+    "thumbnail": "https://i.ibb.co/L1b6xSq/shape.jpg",
+    "title": "Shape of You",
+    "authors": [
+        {
+            "profile_picture": "https://i.ibb.co/D9wWRM6/olivia.jpg",
+            "profile_name": "Olivia Mitchell",
+            "verified": ""
+        }
+    ],
+    "others": {
+        "views": "100K",
+        "posted_date": "16278"
+    },
+    "description": "Dive into the rhythm of 'Shape of You,' a captivating track that blends pop sensibilities with vibrant beats. Created by Olivia Mitchell, this song has already gained 100K views since its release. With its infectious melody and heartfelt lyrics, 'Shape of You' is perfect for fans looking for an uplifting musical experience. Let the music take over as Olivia's vocal prowess and unique style create a memorable listening journey."
+}
+ * 
+ * 
+ * 
+ */
+
+
+const loadDetails = async (id)=>{
+
+        try {
+            // console.log(id);
+            let data = await fetch(`https://openapi.programming-hero.com/api/phero-tube/video/${id}`);
+            data = await data.json();
+            showVideoDetails(data.video)
+           
+            my_modal_1.showModal();
+
+        } catch (error) {
+            console.log(error);
+        }
+}
+
+const showVideoDetails = (video)=>{
+  console.log(video);
+  console.log(video.title);
+  console.log(video.description);
+
+  const div = document.createElement('div');
+  my_modal_1.innerHTML = "";
+  div.innerHTML = `
+  
+ <div class="bg-white flex flex-col space-y-4 lg:w-96 h-auto mx-auto p-4">
+    <!-- image thumbnail -->
+    <div class="relative overflow-hidden">
+        <img class="object-cover w-full h-48" src="${video.thumbnail}" alt="Thumbnail">
+        ${video?.others?.posted_date.length !== 0 ? `
+        <span class="text-sm absolute right-3 bottom-3 text-white bg-black px-2 py-1 rounded-lg">${video?.others?.posted_date || ''}</span>
+        ` : ""}
+    </div>
+    <!-- text_div -->
+    <div class="flex flex-col justify-center items-center gap-2 p-2">
+        <!-- profile -->
+        <h3 class="text-2xl font-bold break-words text-center">${video.title}</h3>
+       
+        <!-- text -->
+        <div class="flex flex-col justify-center items-center space-y-2">
+            <div class="w-16 h-16 overflow-hidden">
+                <img class="rounded-full object-cover w-full h-full" src="${video.authors[0].profile_picture}" alt="Profile Picture">
+            </div>
+            <div class="flex items-center gap-2">
+                <p class="text-gray-500">${video.authors[0].profile_name}</p>
+                ${video.authors[0].verified ? `<img class="w-5" src="./verify.png" alt="Verified">` : ""}
+            </div>
+            <div class="flex space-x-2">
+                <p class="text-gray-500">${video.others.views} Views</p>
+            </div>
+            <p class="text-gray-600 hover:text-blue-800 text-justify overflow-auto">
+                ${video.description}
+            </p>
+        </div>
+    </div>
+    <!-- modal action -->
+<div class="modal-action ">
+    <form method="dialog" class="">
+        <button class="border-none btn hover:text-white hover:bg-gradient-to-r hover:from-fuchsia-500 hover:to-cyan-500   rounded-md py-1 px-4  cursor-pointer">Close</button>
+    </form>
+</div>
+</div>
+
+
+
+  
+  `
+
+  my_modal_1.appendChild(div);
 }
 
 
@@ -155,11 +250,26 @@ if (years > 0 ) {
 const displayVideos = (videos)=>{
 
     if(videos === false){
-        video_section.innerHTML = `<p>No videos found</p>`
+        video_section.innerHTML = "";
+        no_video_section.innerHTML = `<div class="w-full h-full ">
+        <div class=" flex flex-col justify-center items-center space-y-4" id="no_video">
+                <!-- image -->
+                <div class="flex justify-center items-center">
+                    <img src="./Icon.png" alt="">
+                </div>
+                <div class="flex flex-col justify-center items-center">
+                    <h1 class="text-2xl font-bold">Opp! Sorry 😰, No video found</h1> <br>
+                
+                </div>
+            </div>
+        
+        </div>`;
     }else{
         video_section.innerHTML = ""
+        no_video_section.innerHTML = "";
         videos.map((item)=> {
             
+            // console.log(item)
             let localdate = "";
             if(item?.others?.posted_date.length){
                 
@@ -167,38 +277,38 @@ const displayVideos = (videos)=>{
             }
             
               const video_item = document.createElement("div");
-      
-            
-            
-                video_section.classList = "w-11/12  grid grid-col-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-4 mx-auto justify-center items-center my-6 space-y-10 space-x-4 ";
+              video_section.classList = "w-11/12  grid grid-col-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-4 mx-auto justify-center items-center my-6 space-y-10 space-x-4 ";
                     video_item.innerHTML = `
                     
-                <div class="relative  w-fit bg-gradient-to-r from-fuchsia-500 to-cyan-500 rounded-lg  hover:shadow-lg">
-                    <div class="bg-white flex-col space-y-4  w-64 h-76 mx-auto  hover:scale-95 hover:cursor-pointer transition-all duration-500">
-                                        <!-- image thumbail -->
-                                        <div class="overflow relative" >
-                                                <img class="object-cover overflow w-64 h-48" src="${item.thumbnail}" alt="">
-                                               ${item?.others?.posted_date.length !== 0 ? ` <span class="text-base absolute right-3 bottom-3 text-white bg-black px-2 py-1 rounded-lg">${localdate}</span>` : ""}
-                                        </div>
-                                        <!-- text_div -->
-                                        <div class="flex justify-start items-start gap-2 p-2">
-                                            <!-- profile -->
-                                            <div class="w-10 h-10 overflow-hidden">
-                                                    <img class="rounded-full object-cover w-full h-full" src="${item.authors[0].profile_picture}" alt="">
+                  <div class="relative  w-fit bg-gradient-to-r from-fuchsia-500 to-cyan-500 rounded-lg  hover:shadow-lg">
+                        <div class="bg-white flex-col space-y-4  w-64 h-76 mx-auto  hover:scale-95 hover:cursor-pointer transition-all duration-500">
+                                            <!-- image thumbail -->
+                                            <div class="overflow relative" >
+                                                    <img class="object-cover overflow w-64 h-48" src="${item.thumbnail}" alt="">
+                                                   ${item?.others?.posted_date.length !== 0 ? ` <span class="text-base absolute right-3 bottom-3 text-white bg-black px-2 py-1 rounded-lg">${localdate}</span>` : ""}
                                             </div>
-                                            <!-- text -->
-                                            <div class="">
-                                                <h3 class="font-bold  break-normal">${item.title}</h3>
-        
-                                                <div class="flex  items-center gap-2"> 
-                                                    <p class="text-gray-400">${item.authors[0].profile_name}</p>
-                                                <img class="w-5" src="${item.authors[0].verified ? "./verify.png" : "" }" alt=""></img>
+                                            <!-- text_div -->
+                                            <div class="flex justify-start items-start gap-2 p-2">
+                                                <!-- profile -->
+                                                <div class="w-10 h-10 overflow-hidden">
+                                                        <img class="rounded-full object-cover w-full h-full" src="${item.authors[0].profile_picture}" alt="">
                                                 </div>
-                                                <p class="text-gray-400">${item.others.views} Views</p>
+                                                <!-- text -->
+                                                <div class="">
+                                                    <h3 class="font-bold  break-normal">${item.title}</h3>
+            
+                                                    <div class="flex  items-center gap-2"> 
+                                                        <p class="text-gray-400">${item.authors[0].profile_name}</p>
+                                                    <img class="w-5" src="${item.authors[0].verified ? "./verify.png" : "" }" alt=""></img>
+                                                    </div>
+                                                 <div class="flex space-x-2">
+                                                    <p class="text-gray-400">${item.others.views} Views</p>
+                                                    <p onclick="loadDetails('${item.video_id}')" class="text-gray-40 hover:text-blue-800 border-b-2 border-transparent hover:border-blue-700" type="button">See Details</p>
+                                                 </div>
+                                                </div>
                                             </div>
                                         </div>
-                                    </div>
-                                    </div>
+                                        </div>
                             `;
         
                 video_section.appendChild(video_item);
